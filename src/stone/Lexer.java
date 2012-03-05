8f0eb692453d903609c2a6ef4598563ba1435db9
RytaroTsuji <rt.kametaro@gmail.com>
2012-03-05 14:00:24 +0900

    creating Lexer class

--- src/stone/Lexer.java
+++ src/stone/Lexer.java
@@ -2,6 +2,7 @@ package stone;
 
 import java.io.IOException;
 import java.io.LineNumberReader;
+import java.io.Reader;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,12 +19,16 @@ public class Lexer {
 	
 	public Lexer(Reader r) {
 		hasMore = true;
-		reader = new LineNumberReader();
+		reader = new LineNumberReader(r);
 	}
 	public Token read() throws ParseException {
-		if (fillQeueue(0)) {
+		if (fillQueue(0)) {
 			return queue.remove(0);
-		}	
+		}
+		else
+		{
+			return Token.EOF;
+		}
 	}
 	public Token peak(int i) throws ParseException {
 		if (fillQueue(i)) {
@@ -65,5 +70,59 @@ public class Lexer {
 		while (pos < endPos) {
 			matcher.region(pos, endPos);
 		}
+		
+		if(matcher.lookingAt()) {
+			addToken(lineNo , matcher);
+			pos = matcher.end();
+		}
+		else
+		{
+			throw new ParseException("bad token at line " + lineNo);
+		}
+		queue.add(new IdToken(lineNo , Token.EOL));
 	}
+	
+	protected void addToken(int lineNo , Matcher matcher) {
+		String m = matcher.group(1);
+		
+		if(m != null) {
+			if (matcher.group(2) == null) {
+				Token token;
+				
+				if (matcher.group(3) != null) {
+					token = new NumToken(lineNo , Integer.parseInt(m));
+				}
+				else if (matcher.group(4) != null) {
+					token = new StrToken(lineNo , toStringLiteral(m));
+				}
+				else
+				{
+					token = new IdToken(lineNo , m)
+				}
+				queue.add(token);
+			}
+		}
+	}
+	
+	protected String toStringLiteral(String s) {
+		StringBuilder sb = new StringBuilder();
+		int len = s.length() -1;
+		for(int i = 1; i < len; i++){
+			char c = s.charAt(i);
+			if(c == '\\' && i + 1 < len) {
+				int c2 = s.charAt(i + 1);
+				if (c2 == '"' || c2 == '\\') {
+					c = s.charAt(++i);
+				}
+				else if (c2 == 'n')
+				{
+					++i;
+					c = '\n';
+				}
+			}
+			sb.append(c);
+		}
+		return sb.toString();
+	}
+	
 }
